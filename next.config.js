12a7749f5459860d37557da7b0905a66a4d414cf
Jonathan Hudak <jmhudak@amazon.com>
2022-12-23 15:08:10 -0800

    Expose public env variables in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const { env } = process;
+
+const publicEnvVars = Object.keys(env).filter(k => k.startsWith('NEXT_PUBLIC_')).reduce((acc, k) => ({
+  ...acc,
+  [k]: env[k],
+}), {});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: publicEnvVars,
+};
 
 module.exports = nextConfig
